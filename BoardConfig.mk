d9862453a535c572ac62e4ff4e89991dbec628c8
tosasitill <tosasitill@users.noreply.github.com>
2026-07-27 13:43:10 +0800

    Use supported armv8 architecture variant

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -3,7 +3,7 @@ DEVICE_PATH := device/oppo/findx9
 include $(DEVICE_PATH)/configs/BoardConfigTwrp.mk
 
 TARGET_ARCH := arm64
-TARGET_ARCH_VARIANT := armv9-a
+TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_VARIANT := generic
 TARGET_CPU_ABI := arm64-v8a
 TARGET_CPU_ABI2 :=
