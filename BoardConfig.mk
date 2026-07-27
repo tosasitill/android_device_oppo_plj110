3083cc9f1db5ecf7929a7af24ce60e017500db3f
tosasitill <tosasitill@users.noreply.github.com>
2026-07-27 13:32:33 +0800

    Define arm64 CPU ABI for Android build

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,6 +5,8 @@ include $(DEVICE_PATH)/configs/BoardConfigTwrp.mk
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv9-a
 TARGET_CPU_VARIANT := generic
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
 
 TARGET_BOARD_PLATFORM := mt6993
 TARGET_BOARD_PLATFORM_GPU := immortalis
