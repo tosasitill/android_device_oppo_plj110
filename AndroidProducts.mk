9d0b4f8f9fd646853f4cc0c2c65789dc121be6f5
tosasitill <tosasitill@users.noreply.github.com>
2026-07-27 15:27:05 +0800

    Use TWRP-prefixed product required by TWRP 14.1

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,2 +1,2 @@
-PRODUCT_MAKEFILES := $(LOCAL_DIR)/omni_findx9.mk
-COMMON_LUNCH_CHOICES := omni_findx9-eng
+PRODUCT_MAKEFILES := $(LOCAL_DIR)/twrp_findx9.mk
+COMMON_LUNCH_CHOICES := twrp_findx9-eng
--- twrp_findx9.mk
+++ twrp_findx9.mk
@@ -2,7 +2,7 @@ $(call inherit-product, vendor/twrp/config/common.mk)
 $(call inherit-product, device/oppo/findx9/device.mk)
 
 PRODUCT_DEVICE := findx9
-PRODUCT_NAME := omni_findx9
+PRODUCT_NAME := twrp_findx9
 PRODUCT_BRAND := OPPO
 PRODUCT_MODEL := OPPO Find X9
 PRODUCT_MANUFACTURER := OPPO
